4f68606a865e3172a6c77364b372bf688123193e
gaborit321-hue <gaborit321@gmail.com>
2026-03-31 12:00:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const VERSION = 'v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', () => {});
