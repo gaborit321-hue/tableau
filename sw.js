2ba1abfc352c04036ad02bc676a69f19ff881b49
gaborit321-hue <gaborit321@gmail.com>
2026-03-31 13:41:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,30 @@
-const VERSION = 'v1';
+const VERSION = 'v2';
+const CACHE = 'le-tableau-' + VERSION;
 
-self.addEventListener('install', () => self.skipWaiting());
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', () => {});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
